d73d980385bbc00a1e09926a1448cd8c967ee732
linyunxiao <linyunxiao@users.noreply.github.com>
2022-05-20 13:25:17 +0800

    Update menu_item.dart

--- plugins/menubar/lib/src/menu_item.dart
+++ plugins/menubar/lib/src/menu_item.dart
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import 'package:flutter/widgets.dart';
+import 'package:flutter/widgets.dart' hide MenuItem;
 
 /// A callback provided to [MenuItem] to handle menu selection.
 typedef MenuSelectedCallback = void Function();
